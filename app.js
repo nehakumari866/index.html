c35e9e4de4d8053bd1e1fe3527ced46d8864f629
nehakumari866 <nehakumaristm9933@gmail.com>
2026-05-28 23:47:47 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function fun(){
+  alter("click Hua, opening Google");
+  window.location="https://google.com";
+}
+function fun(){
+  alert("click Hua,opening facebook");
+  window.location="https:// facebook.com";
+}
+function git(){
+  alert("click Hua,opening GitHub");
+  window.location="https://GitHub.com"
+}
